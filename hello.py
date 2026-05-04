0122ea01df751770c6e266af656f27501e115ae4
apoorvapatil803-debug <apoorvapatil803@gmail.com>
2026-05-04 09:50:26 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
-#test2
-#retriggering
-# updated
-import os
-import sys
-x=1+1
-def hello( ):
-    print("hello world")
-    y = 10
-hello()
+import sqlite3
+
+def get_user(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    cursor.execute(query)
+    return cursor.fetchall()
